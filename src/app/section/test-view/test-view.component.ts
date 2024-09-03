c9fc0ee428b2f35050cfb658644f774e58ac2d1c
Guti <gutifer666@gmail.com>
2024-09-03 08:22:52 +0200

    formated files

--- src/app/section/test-view/test-view.component.ts
+++ src/app/section/test-view/test-view.component.ts
@@ -1,33 +1,33 @@
 import {Component, OnInit} from '@angular/core';
 import {LocalFileTestRepository} from "../../module/test/infrastructure/localFileTestRepository";
-import { CardModule } from 'primeng/card';
+import {CardModule} from 'primeng/card';
 import {QuestionViewComponent} from "../question-view/question-view.component";
 import {Test} from "../../module/test/domain/Test";
 import {ActivatedRoute, Router} from "@angular/router";
 import {NgIf} from "@angular/common";
 
 @Component({
-  selector: 'app-test-view',
-  standalone: true,
-  imports: [CardModule, QuestionViewComponent, NgIf],
-  templateUrl: './test-view.component.html',
-  styleUrl: './test-view.component.scss'
+    selector: 'app-test-view',
+    standalone: true,
+    imports: [CardModule, QuestionViewComponent, NgIf],
+    templateUrl: './test-view.component.html',
+    styleUrl: './test-view.component.scss'
 })
 export class TestViewComponent implements OnInit {
-  test: Test | undefined;
+    test: Test | undefined;
 
-  constructor(private router: Router, private route: ActivatedRoute,private testService: LocalFileTestRepository) {
-  }
+    constructor(private router: Router, private route: ActivatedRoute, private testService: LocalFileTestRepository) {
+    }
 
-ngOnInit() {
-  const routeParams = this.route.snapshot.paramMap;
-  this.test = this.testService.createTest(
-    routeParams.get('subject')!,
-    routeParams.get('evaluation')!,
-    parseInt(routeParams.get('percentageOfQuestions')!));
-}
+    ngOnInit() {
+        const routeParams = this.route.snapshot.paramMap;
+        this.test = this.testService.createTest(
+            routeParams.get('subject')!,
+            routeParams.get('evaluation')!,
+            parseInt(routeParams.get('percentageOfQuestions')!));
+    }
 
-  navigateToTestMakerView() {
-    this.router.navigate(['']).then(r => console.log(r));
-  }
+    navigateToTestMakerView() {
+        this.router.navigate(['']).then(r => console.log(r));
+    }
 }

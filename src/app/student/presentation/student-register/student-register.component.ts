6b924eece600d768c1d25f3ab83c8383a76a7271
Guti <gutifer666@gmail.com>
2024-09-15 13:08:52 +0200

    feat: implement reactive register form with ng-prime

--- src/app/student/presentation/student-register/student-register.component.ts
+++ src/app/student/presentation/student-register/student-register.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {FormBuilder, ReactiveFormsModule, Validators} from "@angular/forms";
+import {InputTextModule} from "primeng/inputtext";
+import {Button} from "primeng/button";
+import {FloatLabelModule} from "primeng/floatlabel";
 
 @Component({
   selector: 'app-student-register',
   standalone: true,
-  imports: [],
+    imports: [
+        ReactiveFormsModule,
+        InputTextModule,
+        Button,
+        FloatLabelModule
+    ],
   templateUrl: './student-register.component.html',
   styleUrl: './student-register.component.scss'
 })
 export class StudentRegisterComponent {
+    private fb = inject(FormBuilder);
+    public registerForm = this.fb.group({
+        name: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', Validators.required]
+    });
 
+    onSubmit() {
+        console.log(this.registerForm.value);
+    }
 }
